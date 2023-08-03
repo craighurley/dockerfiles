3cda3081a2984ba4d5938d8f661b3207921ce539
Craig Hurley <craig@thehurley.com>
2023-08-03 18:04:27 +1200

    Pull shellcheck

--- pull.sh
+++ pull.sh
@@ -8,6 +8,7 @@ docker pull craighurley/cfn-nag:latest
 docker pull craighurley/markdownlint-cli:latest
 docker pull craighurley/org-formation-cli:latest
 docker pull craighurley/sceptre:latest
+docker pull craighurley/shellcheck:latest
 docker pull craighurley/yamllint:latest
 
 docker rmi "$(docker images -f 'dangling=true' -q)"
